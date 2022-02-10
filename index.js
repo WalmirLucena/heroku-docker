b050fb937e0b11e190cf2d130eb4e5b7aaec20f8
WalmirLucena <walmirlucena3@gmail.com>
2022-02-10 14:53:37 -0400

    Arquivo com deploy

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => res.send(`Executando na porta: ${port}`));
+
+app.listen(port, () => console.log(`online aqui ${port}`));
